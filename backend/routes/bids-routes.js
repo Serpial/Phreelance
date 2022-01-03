ec0d603368ae0ab16c4d0aafd4030e4d8a6e1fa1
Serpial <phutchison8@gmail.com>
2022-01-03 01:22:05 +0000

    dummy bid backend

--- backend/routes/bids-routes.js
+++ backend/routes/bids-routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
+const bidsController = require("../controllers/bids-controller");
+
 const router = express.Router();
 
-router.get("/:userID");
-router.post("/:userID");
-router.patch("/:userID");
-router.delete("/:userID");
-
-router.get("/auction/:auctionID");
+router.get("/user/:userID", bidsController.getBidsByUser);
+router.post("/create/:userID", bidsController.createBidForUser);
+router.get("/auction/:auctionID", bidsController.getBidsByAuction);
+router.get("/:bidID", bidsController.getBid);
+router.patch("/:bidID", bidsController.updateBid);
+router.delete("/:bidID", bidsController.deleteBid);
 
 module.exports = router;
