0b5f026f255d1fbffd8c675cd06dbd9d584b186b
Serpial <phutchison8@gmail.com>
2022-01-29 16:10:35 +0000

    change auth folder name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,9 @@ import {
 
 import AuthOutlet from "./shared/outlets/AuthOutlet";
 import MainOutlet from "./shared/outlets/MainOutlet";
-import Login from "./authentication/pages/Login";
-import Register from "./authentication/pages/Register";
-import Forgot from "./authentication/pages/Forgot";
+import Login from "./auth/pages/Login";
+import Register from "./auth/pages/Register";
+import Forgot from "./auth/pages/Forgot";
 import Auctions from "./auctions/pages/Auctions";
 import { AuthProvider, useAuth } from "./shared/contexts/AuthContext";
 
