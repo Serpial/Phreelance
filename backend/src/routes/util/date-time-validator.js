5659ae788fe7a1023a49c17404c06520226a91bc
Paul Hutchison SE2017 <paul.hutchison.2017@uni.strath.ac.uk>
2022-02-12 01:38:45 +0000

    remove back end and move forward the frontend

--- backend/src/routes/util/date-time-validator.js
+++
@@ -1,7 +0,0 @@
-// Example: 16 Feb 2022 12:30:00 GMT
-const dateTimeValidator = (input) => {
-  const dateTime = /^\d{1,2} \w{3} \d{4} \d{2}:\d{2}:\d{2} \w{3}$/;
-  return dateTime.test(input);
-};
-
-module.exports = dateTimeValidator;
