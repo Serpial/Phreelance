f4563cf77890e03a3434e60b4c8ef99076468a5c
Serpial <phutchison8@gmail.com>
2022-03-10 16:10:28 +0000

    Add tooltips for icons

--- src/auctions/components/AuctionCard.js
+++ src/auctions/components/AuctionCard.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Card from "react-bootstrap/Card";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUserCircle,
   faExclamationCircle,
 } from "@fortawesome/free-solid-svg-icons";
 
+import FindTimeBetween from "../util/FindTimeBetween";
 import BasicCard from "../../shared/components/BasicCard";
 
 import "./AuctionCard.css";
@@ -105,8 +108,8 @@ const AuctionCard = (props) => {
 
     if (!hasElapsed) {
       const timeBefore = hasStarted
-        ? findTimeBetween(now, closeDateTime)
-        : findTimeBetween(now, startDateTime);
+        ? FindTimeBetween(now, closeDateTime)
+        : FindTimeBetween(now, startDateTime);
       setTimeRemaining(timeBefore);
     }
     setDoTimeRefresh(false);
@@ -116,6 +119,14 @@ const AuctionCard = (props) => {
     description.length < 120
       ? description
       : description.slice(0, 120).trim() + "...";
+
+  const topBidderTooltip = (props) => (
+    <Tooltip {...props}>You are the top bidder!</Tooltip>
+  );
+
+  const notTopTooltip = (props) => (
+    <Tooltip {...props}>You are no longer top bidder!</Tooltip>
+  );
 
   return (
     <BasicCard className="auction-card">
@@ -166,20 +177,32 @@ const AuctionCard = (props) => {
                 {minBid}
                 <span className="auction-card_notification">
                   {started && topBidIsUser && (
-                    <FontAwesomeIcon
-                      className="auction-card_icon"
-                      icon={faUserCircle}
-                      fixedWidth
-                      size="lg"
-                    />
+                    <OverlayTrigger
+                      placement="top"
+                      delay={{ show: 250, hide: 400 }}
+                      overlay={topBidderTooltip}
+                    >
+                      <FontAwesomeIcon
+                        className="auction-card_icon"
+                        icon={faUserCircle}
+                        fixedWidth
+                        size="lg"
+                      />
+                    </OverlayTrigger>
                   )}
                   {userBid && !elapsed && !topBidIsUser && (
-                    <FontAwesomeIcon
-                      className="auction-card_icon auction-card_warning-icon"
-                      icon={faExclamationCircle}
-                      fixedWidth
-                      size="lg"
-                    />
+                    <OverlayTrigger
+                      placement="top"
+                      delay={{ show: 250, hide: 400 }}
+                      overlay={notTopTooltip}
+                    >
+                      <FontAwesomeIcon
+                        className="auction-card_icon auction-card_warning-icon"
+                        icon={faExclamationCircle}
+                        fixedWidth
+                        size="lg"
+                      />
+                    </OverlayTrigger>
                   )}
                 </span>
               </span>
@@ -207,32 +230,6 @@ const parseBid = (valueNumber) => {
   });
 
   return numberFormat.format(valueNumber);
-};
-
-const findTimeBetween = (before, after) => {
-  const difference = after - before;
-  const difference_in_months = difference / (1000 * 3600 * 24 * 31);
-  if (difference_in_months > 1) {
-    return Math.floor(difference_in_months) + " months";
-  }
-
-  const difference_in_days = difference_in_months * 31;
-  if (difference_in_days > 1) {
-    return Math.floor(difference_in_days) + " days";
-  }
-
-  const difference_in_hours = difference_in_days * 24;
-  if (difference_in_hours > 1) {
-    return Math.floor(difference_in_hours) + " hours";
-  }
-
-  const difference_in_minutes = difference_in_hours * 60;
-  if (difference_in_minutes > 1) {
-    return Math.floor(difference_in_minutes) + " minutes";
-  }
-
-  const difference_in_seconds = difference_in_minutes * 60;
-  return Math.floor(difference_in_seconds) + " seconds";
 };
 
 export default AuctionCard;
